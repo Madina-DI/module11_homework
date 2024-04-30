f8c2d06a91f603fc460832715e041e03eb07966e
Madina_DI <12mileik@rambler.ru>
2024-04-30 14:31:40 +0300

    first commit

---
+++ learn.js
@@ -0,0 +1,4 @@
+const firstName = prompt('введите ваше имя');
+const lastName = prompt('введите вашу фамилию');
+const fullName = firstName  + ' ' + lastName;
+alert('Привет, ты чудо ' + fullName + '!');
